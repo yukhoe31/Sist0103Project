ce2c3ca4d477595bf33fb8a875c0ea8c0d18ba8f
lmy <yukhoe31@gmail.com>
2024-01-25 16:40:34 +0900

    update 0125 jdbc

--- JavaProject/JdbcProject/src/day0125/ConnectTest.java
+++ JavaProject/JdbcProject/src/day0125/ConnectTest.java
@@ -9,32 +9,32 @@ import java.sql.SQLException;
 public class ConnectTest {
 
 	static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
-	
+
 	public void connectSawon() {
-		
+
 		//아래내용 자동완성시 -> 전부 .sql 버전으로 하기.
 		Connection conn = null; //초기값 없으니까 null해준다.
 		java.sql.Statement stmt = null;//
 		ResultSet rs = null;//
-		
+
 		String sql = "select * from sawon order by num";
 		//num의 오름차순으로 사원 데이터를 가지고 오겠다.
 		//sawon 테이블은 day0124에 stu계정으로 만들었으므로
 		//집에서 다시 할 땐, sawon테이블부터 있는지 확인할것.
-		
+
 		try {
 			conn = DriverManager.getConnection(URL, "stu", "a1234");
-			
+
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery(sql);
 			//rs: 테이블 객체를 그대로 가져오는 api
-			
+
 			//여러줄을 DB에서 가져올 때는 while문을 쓴다.
 			//true값을 반환...false가 되는순간 빠져나와.
 			//rs.next():다음 데이터를 이동하면서 true반환.
 			//더이상 데이터가 없으면 false 반환
 			while(rs.next()) {
-				
+
 				//DB로부터 데이터 가져오기
 				int num = rs.getInt("num");		
 				//int num = rs.getInt(1);  //이렇게도 되긴함.
@@ -42,33 +42,92 @@ public class ConnectTest {
 				String gender = rs.getString("gender");
 				String buseo = rs.getString("buseo");
 				int pay = rs.getInt("pay");
-				
+
 				System.out.println(num + "\t"+ name+"\t" 
-				+ gender+"\t"+buseo+"\t"+pay);
+						+ gender+"\t"+buseo+"\t"+pay);
 			}
-			
+
 			System.out.println("오라클 드라이버 연결 성공!!!");
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			//e.printStackTrace();
 			System.out.println("오라클 드라이버 연결 실패!!!");
 		}finally {
-				try {
-					if(rs!=null) rs.close();
-					if(stmt!=null) stmt.close();
-					if(conn!=null) conn.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			
+			try {
+				if(rs!=null) rs.close();
+				if(stmt!=null) stmt.close();
+				if(conn!=null) conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
 	}
-	
-	public static void main(String[] args) {
+
+	public void connectShop() {
+
+		Connection conn = null;
+		java.sql.Statement stmt = null;
+		ResultSet rs = null;
+
+		String sql = "select idx,c.num,sangpum,color,cnt,guipday from shop s,cart2 c where s.num=c.num\r\n";
 		
+		try {
+			conn = DriverManager.getConnection(URL, "stu","a1234");
+			System.out.println("오라클 성공★");
+
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);//executeQuery를 통해서 sql문을 전달
+
+			System.out.println("**회원님 장바구니 리스트**");
+			System.out.println("주문번호\t상품번호\t상품명\t\t색상\t구입갯수\t구입날짜");
+			System.out.println("-------------------------------------"
+					+ "----------------------------------------------");
+
+			while(rs.next()) {
+
+				int idx = rs.getInt("idx");
+				int num = rs.getInt("num");
+				String sang = rs.getString("sangpum");
+				String color = rs.getString("color");
+				int cnt = rs.getInt("cnt");
+				String guipday = rs.getString("guipday");
+
+				System.out.println(idx +"\t\t"+ num +"\t\t"+
+						sang +"\t"+ color +"\t"+cnt+"\t\t"+guipday);
+
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			System.out.println("오라클 실패 ♣");
+		}finally {
+			try {
+				if(rs!=null) rs.close();
+				if(stmt!=null) stmt.close();
+				if(conn!=null) conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+
+	public static void main(String[] args) {
+
 		ConnectTest ct = new ConnectTest();
+		
+		//stu계정 내의 sawon테이블 가져오기
 		ct.connectSawon();
+		
+		System.out.println();
+		
+		//stu계정 내의 shop테이블 가져오기
+		ct.connectShop();
 	}
 }
