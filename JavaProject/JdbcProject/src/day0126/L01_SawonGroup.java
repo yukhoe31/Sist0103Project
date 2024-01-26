6213dcf92f414bbacea5acdda8380c296f4b0240
lmy <yukhoe31@gmail.com>
2024-01-26 10:34:44 +0900

    update 0126

---
+++ JavaProject/JdbcProject/src/day0126/L01_SawonGroup.java
@@ -0,0 +1,120 @@
+package day0126;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.w3c.dom.css.CSSRule;
+
+import oracleDb.DbConnect;
+
+public class L01_SawonGroup {
+
+	DbConnect db = new DbConnect();
+
+	public void connectSawonGroup() {
+
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		String sql = "select gender"
+				+ ",count(buseo) count"//count는 alias
+				+ ",to_char(round(avg(pay),0),'L999,999,999') avgpay"
+				+ ",to_char(max(pay),'L999,999,999') maxpay"
+				+ ",to_char(min(pay),'L999,999,999') minpay"
+				+ " from sawon group by gender";
+
+		conn=db.getOracle();
+		System.out.println("오라클 계정연결성공!!!");
+
+
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			System.out.println("\t**회사 남녀별 급여통계**");
+			System.out.println();
+			System.out.println("성별\t인원수\t   평균급여\t\t최고급여\t\t최저급여");
+			System.out.println("----------------------------------"
+					+ "-------------------------------------------------");
+
+			while(rs.next()) {
+				String gender = rs.getString("gender");
+				int count = rs.getInt("count");
+				String avgpay = rs.getString("avgpay");
+				String maxpay = rs.getString("maxpay");
+				String minpay = rs.getString("minpay");
+
+				System.out.println(gender+"\t"+count+" "+avgpay
+						+" "+maxpay+" "+minpay);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(rs, stmt, conn);
+		}
+
+	}
+	
+	public void personGroup() {
+		
+		//job별 인원수와 평균나이 구해주세요.
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		String sql = "select job"
+				+ ",count(job) count"//count는 alias
+				+ ",to_char(round(avg(age),0)) avgage"
+				+ " from person group by job";
+
+		conn=db.getOracle();
+		System.out.println("오라클 계정연결성공!!!");
+		
+		
+		
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			
+			System.out.println("\n**직업별 인원수와 나이**");
+			System.out.println();
+			System.out.println("직업\t\t인원수\t나이");
+			System.out.println("----------------------------------");
+
+			while(rs.next()) {
+				String job = rs.getString("job");
+				int count = rs.getInt("count");
+				String avgage = rs.getString("avgage");
+				
+
+				System.out.println(job+"\t"+count+"\t"+avgage);
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		L01_SawonGroup sgroup = new L01_SawonGroup();
+		sgroup.connectSawonGroup();
+		
+		System.out.println();
+		System.out.println();
+		sgroup.personGroup();
+	}
+
+}
--- JavaProject/JdbcProject/src/oracleDb/DbConnect.java
+++ JavaProject/JdbcProject/src/oracleDb/DbConnect.java
@@ -1,5 +1,96 @@
 package oracleDb;
 
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DbConnect {
+
+	//url : 퀀텀DB가서 lmyDB누르고 우클릭->Properties: 주소 복붙하기
+	static final String ORACLE_DB = "jdbc:oracle:thin:@localhost:1521:XE";
+
+	//connect 
+	//아래: Connection타입을 리턴해야
+	public  Connection getOracle() {
+
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(ORACLE_DB, "stu","a1234");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+
+			System.out.println("로칼 오라클 연결 실패!!!");
+
+		}
+
+		return conn;
+	}
+
+	//close메서드를 총4개 만들기
+	//select만 rs(ResultSet)가 들어간다
+	public void dbClose(ResultSet rs, java.sql.Statement stmt, Connection conn) {
+
+		try {
+			if(rs!=null)  rs.close();
+			if(stmt!=null)  stmt.close();
+			if(conn!=null)  conn.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+	//오버로딩
+	//이름 같은데 다르게 쓰임
+	public void dbClose(java.sql.Statement stmt, Connection conn) {
+
+		try {
+		
+			if(stmt!=null)  stmt.close();
+			if(conn!=null)  conn.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	//Prepare Statement 사용1
+	public void dbClose(ResultSet rs, PreparedStatement pstmt, Connection conn) {
+
+		try {
+			if(rs!=null)  rs.close();
+			if(pstmt!=null)  pstmt.close();
+			if(conn!=null)  conn.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
+	//Prepare Statement 사용2
+	public void dbClose(PreparedStatement pstmt, Connection conn) {
+
+		try {
+		
+			if(pstmt!=null)  pstmt.close();
+			if(conn!=null)  conn.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 
 }
