d79ce0f7697330026a252a9c77384a515c2a3271
yukhoe31 <yukhoe31@gmail.com>
2024-02-27 16:33:48 +0900

    update 0227 vuejs

--- jswork/vuework/vue-last/src/App.vue
+++ jswork/vuework/vue-last/src/App.vue
@@ -9,6 +9,8 @@
 </template>
 
 <script>
+//필요한 App만 주석 없이 사용하기.
+//밑에 export default의 components에서도 해당 App 적어주기
 //import FirstApp from './components/FirstApp.vue';
 //import SecondApp from './components/SecondApp.vue';
 //import ThirdApp from './components/ThirdApp.vue';
