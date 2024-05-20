7968df03bee7887e4087031265a3e5a915c16ca2
yukhoe31 <yukhoe31@gmail.com>
2024-05-20 12:30:56 +0900

    update 0520

--- sts4work/Ex3_2_SpringBootThymeleaf/src/main/java/data/controller/HelloThController.java
+++ sts4work/Ex3_2_SpringBootThymeleaf/src/main/java/data/controller/HelloThController.java
@@ -7,40 +7,91 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import data.dto.ShopDto;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HelloThController {
-	
-	List<ShopDto> list = new ArrayList<>();
+
+	List<ShopDto> list=new ArrayList<>();
 	
 	@GetMapping("/")
-	public String home() {
-		
+	public String home()
+	{
 		return "home";
 	}
-
 	
 	@GetMapping("/hello1")
-	public String hello1(Model model) {
-		
+	public String hello1(Model model)
+	{
 		System.out.println("hello1 controller");
 		
-		model.addAttribute("message","Have a Nice Day!");
-		model.addAttribute("today",new Date());
+		model.addAttribute("message", "Have a Nice Day!!!");
+		model.addAttribute("today", new Date());
 		
-		ShopDto dto = ShopDto.builder()
+		ShopDto dto=ShopDto.builder()
 				.num(100)
 				.sangpum("요가매트")
 				.su(10)
 				.dan(25000)
-				.color("purple")
+				.color("black")
 				.build();
-		
-		model.addAttribute("dto",dto);
-		
+				
+		model.addAttribute("dto", dto);
 		return "hello1";
 	}
-
+	
+	
+	@GetMapping("/hello2")
+	public String hello2(String name,int age,Model model)
+	{
+		model.addAttribute("hello2_msg", name+"님의 나이는 "+age+"세 입니다");
+		return "hello2";
+	}
+	
+	
+	@GetMapping("/list")
+	public String list(Model model)
+	{
+		list.add(new ShopDto(1,"핸드크림",10,8000,"black"));
+		list.add(new ShopDto(2,"립글로스",8,12000,"pink"));
+		list.add(new ShopDto(3,"치약",20,11000,"yellow"));
+		list.add(new ShopDto(4,"칫솔",14,7800,"green"));
+		ShopDto dto5=ShopDto.builder()
+				.num(5)
+				.sangpum("텀블러")
+				.su(8)
+				.dan(250000)
+				.color("silver")
+				.build();
+		
+		list.add(dto5);
+		
+		model.addAttribute("list", list);
+		return "list";
+	}
+	
+	@GetMapping("/detail")
+	public String detail(@RequestParam int index,Model model)
+	{
+		model.addAttribute("dto", list.get(index));
+		return "detail";
+	}
+	
+	@GetMapping("/login1")
+	public String login1(HttpSession session)
+	{
+		session.setAttribute("myid", "admin");
+		return "login";
+	}
+	
+	@GetMapping("/login2")
+	public String login2(HttpSession session)
+	{
+		session.setAttribute("myid", "angel");
+		return "login";
+	}
+	
 }
--- sts4work/Ex3_2_SpringBootThymeleaf/src/main/java/data/dto/ShopDto.java
+++ sts4work/Ex3_2_SpringBootThymeleaf/src/main/java/data/dto/ShopDto.java
@@ -5,15 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor //모든 필드값을 파라미터로 받는 생성자를 만듦
+@AllArgsConstructor
 @Builder
 public class ShopDto {
-	
+
 	private int num;
 	private String sangpum;
 	private int su;
 	private int dan;
 	private String color;
-	
 	
 }
