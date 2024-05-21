d6244b74ea052dc4eb6f364ef795f11c62a90900
yukhoe31 <yukhoe31@gmail.com>
2024-05-21 14:25:48 +0900

    update 0521

--- sts4work/Ex3_3_BootThymeleafJpa_1/src/main/java/mycar/controller/MyCarCommentController.java
+++ sts4work/Ex3_3_BootThymeleafJpa_1/src/main/java/mycar/controller/MyCarCommentController.java
@@ -2,7 +2,9 @@ package mycar.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,4 +42,6 @@ public class MyCarCommentController {
 	{
 		return commentDao.getMyCarCommentList(num);
 	}
+
+
 }
